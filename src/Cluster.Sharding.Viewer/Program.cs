5e30d62483c79158a88a95c513ab0cc949f06f5b
Aaron Stannard <aaron@petabridge.com>
2019-03-22 11:36:22 -0500

    completed first implementation of program

--- src/Cluster.Sharding.Viewer/Program.cs
+++ src/Cluster.Sharding.Viewer/Program.cs
@@ -3,6 +3,9 @@ using System.IO;
 using Akka.Actor;
 using Akka.Cluster.Sharding;
 using Akka.Configuration;
+using Akka.Persistence.Query;
+using Akka.Persistence.Query.Sql;
+using Akka.Streams;
 
 namespace Cluster.Sharding.Viewer
 {
@@ -12,10 +15,18 @@ namespace Cluster.Sharding.Viewer
         {
             var hocon = ConfigurationFactory.ParseString(File.ReadAllText("reference.conf"));
             var actorSystem = ActorSystem.Create("ClusterShardingViewer",
-                hocon.WithFallback(ClusterSharding.DefaultConfig()));
+                hocon.WithFallback(ClusterSharding.DefaultConfig())
+                    .WithFallback(SqlReadJournal.DefaultConfiguration()));
             actorSystem.Log.Info("Starting up...");
 
+            var readJournal = actorSystem.ReadJournalFor<SqlReadJournal>(SqlReadJournal.Identifier);
+            var query =  readJournal.CurrentEventsByPersistenceId("/system/sharding/fubersCoordinator/singleton/coordinator", 0,
+                330);
 
+            query.RunForeach(e =>
+            {
+                Console.WriteLine("{0}: {1}", e.SequenceNr, e.Event);
+            }, actorSystem.Materializer());
 
             Console.ReadLine();
         }
